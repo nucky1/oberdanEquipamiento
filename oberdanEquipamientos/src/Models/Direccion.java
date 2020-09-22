26f2319075739d5d58d9487aed42639f019c625f
perezj861 <perezj861@gmail.com>
2020-09-22 15:27:44 -0300

    Intento de pushear cliente direccion y solicitud, junto con los views de abmSolicitudes

---
+++ oberdanEquipamientos/src/Models/Direccion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Hernan
+ */
+public class Direccion {
+
+    int id;
+    int id_barrio;
+    String nombre;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_barrio() {
+        return id_barrio;
+    }
+
+    public void setId_barrio(int id_barrio) {
+        this.id_barrio = id_barrio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
